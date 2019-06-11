7d471073ef154b2e51525ce862e15f994b7dfcee
Oldli <wenju1991@163.com>
2019-06-11 23:17:12 +0800

    update

---
+++ src/Library/Cache/Cache.Redis/RedisHelper.cs
@@ -0,0 +1,22 @@
+﻿using Nm.Lib.Utils.Core;
+using StackExchange.Redis;
+
+namespace Nm.Lib.Cache.Redis
+{
+    public class RedisHelper
+    {
+        private readonly ConnectionMultiplexer _redis;
+        private readonly IDatabase _db;
+        private readonly RedisOptions _options;
+
+        public RedisHelper(RedisOptions options)
+        {
+            Check.NotNull(options.ConnectionString, );
+
+            _options = options;
+
+            _redis = ConnectionMultiplexer.Connect(_options.ConnectionString);
+            _db = _redis.GetDatabase();
+        }
+    }
+}
---
+++ src/Library/Cache/Cache.Redis/RedisOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nm.Lib.Cache.Redis
+{
+    public class RedisOptions
+    {
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
