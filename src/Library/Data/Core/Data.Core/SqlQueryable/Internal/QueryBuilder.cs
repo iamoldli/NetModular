860483fd47c89124b4c40baa5be849829c817f09
oldli <wenju1991@163.com>
2019-06-23 15:53:45 +0800

    修复分组查询别名无效的bug

--- src/Library/Data/Core/Data.Core/SqlQueryable/Internal/QueryBuilder.cs
+++ src/Library/Data/Core/Data.Core/SqlQueryable/Internal/QueryBuilder.cs
@@ -536,7 +536,7 @@ namespace Nm.Lib.Data.Core.SqlQueryable.Internal
                 //分组查询
                 if (_queryBody.IsGroupBy)
                 {
-                    var descriptor = _queryBody.GroupByPropertyList.FirstOrDefault(m => _sqlAdapter.AppendQuote(m.Alias) == alias);
+                    var descriptor = _queryBody.GroupByPropertyList.FirstOrDefault(m => _sqlAdapter.AppendQuote(m.Alias) == alias || m.Name == memberExp.Member.Name);
                     if (descriptor != null)
                     {
                         var colName = _queryBody.GetColumnName(descriptor.Name, descriptor.JoinDescriptor);
