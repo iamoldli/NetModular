55ef649b3d90cdb2508b66e628a03ac3dc4ee918
oldli <wenju1991@163.com>
2019-07-17 21:10:16 +0800

    新增DateTimeHelper类

---
+++ src/Library/Utils/Utils.Core/Helpers/DateTimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nm.Lib.Utils.Core.Attributes;
+
+namespace Nm.Lib.Utils.Core.Helpers
+{
+    [Singleton]
+    public class DateTimeHelper
+    {
+        /// <summary>
+        /// 获取时间戳
+        /// </summary>
+        /// <returns></returns>
+        public string GetTimeStamp()
+        {
+            var ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+    }
+}
