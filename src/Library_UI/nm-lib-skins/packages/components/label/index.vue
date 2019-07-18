d4e56487def71de96a12e3d002e6b03bcec7c6fe
oldli <wenju1991@163.com>
2019-07-18 18:33:44 +0800

    update

---
+++ src/Library_UI/nm-lib-skins/packages/components/label/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <span :class="class_">
+    <slot>{{text}}</slot>
+  </span>
+</template>
+<script>
+export default {
+  name: 'Label',
+  props: {
+    /** 显示文本 */
+    text: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: ''
+    },
+    /** 左边箭头 */
+    arrowed: {
+      type: Boolean,
+      default: false
+    },
+    /** 左侧内箭头 */
+    arrowedIn: {
+      type: Boolean,
+      default: false
+    },
+    /** 右侧箭头 */
+    arrowedRight: {
+      type: Boolean,
+      default: false
+    },
+    /** 右侧内箭头 */
+    arrowedRightIn: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    class_() {
+      return [
+        'nm-label',
+        this.type ? `nm-label-${this.type}` : '',
+        this.arrowed ? 'arrowed' : '',
+        this.arrowedIn ? 'arrowed-in' : '',
+        this.arrowedRight ? 'arrowed-right' : '',
+        this.arrowedRightIn ? 'arrowed-right-in' : ''
+      ]
+    }
+  }
+}
+</script>
