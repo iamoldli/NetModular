edc87e617762bccc2004bfda5187ca64ca7fc6c0
oldli <wenju1991@163.com>
2019-07-31 08:23:25 +0800

    updte

---
+++ src/Library_UI/nm-lib-skins/packages/page/userInfo/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <nm-container>
+    <nm-box page header title="个人信息" icon="user">
+      <el-form label-width="120px">
+        <el-row>
+          <el-col :span="10" :offset="1">
+            <el-form-item label="编号：">
+              <el-input :value="id" disabled />
+            </el-form-item>
+          </el-col>
+          <el-col :span="10">
+            <el-form-item label="用户名：">
+              <el-input :value="name" disabled />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </nm-box>
+  </nm-container>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState('app/account', ['id', 'name'])
+  }
+}
+</script>
