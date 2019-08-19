df5b34ae4095a5f80b1f5f1758d1dd8b41612c28
oldli <wenju1991@163.com>
2019-08-19 16:51:10 +0800

    update

---
+++ src/Module/Quartz/UI/module-quartz/src/views/Job/components/log/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <nm-list-dialog v-bind="dialog" :visible.sync="visible_" @open="onOpen">
+    <nm-list ref="list" v-bind="list">
+      <!--查询条件-->
+      <template v-slot:querybar>
+        <el-form-item label="开始日期：" prop="startDate">
+          <el-date-picker v-model="list.model.startDate" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="结束日期：" prop="endDate">
+          <el-date-picker v-model="list.model.endDate" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+        </el-form-item>
+      </template>
+
+      <template v-slot:col-type="{row}">
+        <el-tag v-if="row.type===0" type="success" effect="dark" size="small">信息</el-tag>
+        <el-tag v-else-if="row.type===1" type="warning" effect="dark" size="small">调试</el-tag>
+        <el-tag v-else type="danger" effect="dark" size="small">异常</el-tag>
+      </template>
+    </nm-list>
+  </nm-list-dialog>
+</template>
+<script>
+import { mixins } from 'nm-lib-skins'
+import api from '../../../../api/Job'
+import cols from './cols'
+export default {
+  mixins: [mixins.dialog],
+  data() {
+    return {
+      list: {
+        noHeader: true,
+        noOperation: true,
+        cols,
+        action: api.log,
+        model: {
+          jobId: '',
+          startDate: '',
+          endDate: ''
+        }
+      }
+    }
+  },
+  props: {
+    job: {
+      type: Object
+    }
+  },
+  computed: {
+    dialog() {
+      return {
+        title: `任务日志(${this.job.name})`,
+        icon: 'log'
+      }
+    }
+  },
+  methods: {
+    onOpen() {
+      this.list.model.jobId = this.job.id
+    }
+  }
+}
+</script>
