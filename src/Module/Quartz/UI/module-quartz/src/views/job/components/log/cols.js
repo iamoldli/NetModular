cb6c912a5bb0b90c4df858208819b217beaa0701
oldli <wenju1991@163.com>
2019-10-16 08:24:19 +0800

    update

---
+++ src/Module/Quartz/UI/module-quartz/src/views/job/components/log/cols.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: 'id',
+    label: '编号',
+    show: false
+  },
+  {
+    name: 'type',
+    label: '类型',
+    width: 120
+  },
+  {
+    name: 'createdTime',
+    label: '时间',
+    width: 150
+  },
+  {
+    name: 'msg',
+    label: '信息',
+    align: 'left',
+    showOverflowTooltip: true
+  }
+]
