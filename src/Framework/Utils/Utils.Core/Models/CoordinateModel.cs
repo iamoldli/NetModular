1ab121821bcacfebf6051961156694491cf0eeca
Liwj <wenju1991@163.com>
2019-12-09 14:30:42 +0800

    新增坐标模型类CoordinateModel.cs

---
+++ src/Framework/Utils/Utils.Core/Models/CoordinateModel.cs
@@ -0,0 +1,18 @@
+﻿namespace NetModular.Lib.Utils.Core.Models
+{
+    /// <summary>
+    /// 坐标模型
+    /// </summary>
+    public class CoordinateModel
+    {
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string Lng { get; set; }
+
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public string Lat { get; set; }
+    }
+}
