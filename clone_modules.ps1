7389527d3adf9d80cc2941e51355be9f66e08570
Liwj <wenju1991@163.com>
2019-12-28 09:43:09 +0800

    update

--- clone_modules.ps1
+++
@@ -1,7 +0,0 @@
-# 批量clone代码
-
-cd ../
-git clone https://github.com/iamoldli/NetModular.Module.Common.git
-git clone https://github.com/iamoldli/NetModular.Module.CodeGenerator.git
-git clone https://github.com/iamoldli/NetModular.Module.Quartz.git
-git clone https://github.com/iamoldli/NetModular.Module.PersonnelFiles.git
