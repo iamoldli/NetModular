e78c60f749aa56f30ab5e860e285ce760a8efd5b
Liwj <wenju1991@163.com>
2019-12-29 16:07:05 +0800

    添加Sex通用枚举

---
+++ src/Framework/Utils/Utils.Core/Enums/Sex.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace NetModular.Lib.Utils.Core.Enums
+{
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public enum Sex
+    {
+        /// <summary>
+        /// 未知
+        /// </summary>
+        [Description("未知")]
+        UnKnown = -1,
+        [Description("男")]
+        Boy,
+        [Description("女")]
+        Girl
+    }
+}
