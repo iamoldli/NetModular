25b822e4529886275601ec98c0b7e27e8ba2a705
Liwj <wenju1991@163.com>
2020-01-06 18:08:15 +0800

    配置管理添加日志

--- src/Framework/Config/Config.Core/ConfigContainer.cs
+++ src/Framework/Config/Config.Core/ConfigContainer.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using NetModular.Lib.Cache.Abstractions;
 using NetModular.Lib.Config.Abstraction;
 using NetModular.Lib.Module.Abstractions;
@@ -18,12 +19,14 @@ namespace NetModular.Lib.Config.Core
         private readonly ICacheHandler _cacheHandler;
         private readonly IConfigStorage _storage;
         private readonly IModuleCollection _moduleCollection;
+        private readonly ILogger _logger;
 
-        public ConfigContainer(ICacheHandler cacheHandler, IConfigStorage storage, IModuleCollection moduleCollection)
+        public ConfigContainer(ICacheHandler cacheHandler, IConfigStorage storage, IModuleCollection moduleCollection, ILogger<ConfigContainer> logger)
         {
             _cacheHandler = cacheHandler;
             _storage = storage;
             _moduleCollection = moduleCollection;
+            _logger = logger;
         }
 
         public async Task<T> Resolve<T>() where T : IConfig, new()
@@ -73,11 +76,15 @@ namespace NetModular.Lib.Config.Core
         {
             if (_moduleCollection != null)
             {
+                _logger.LogInformation("刷新所有配置项");
+
                 //查询所有
                 var list = await _storage.GetAll();
 
                 foreach (var module in _moduleCollection)
                 {
+                    _logger.LogInformation("刷新配置项：{@name}", module.Name);
+
                     var configTypes = module.AssemblyDescriptor.Infrastructure.GetTypes().Where(m => typeof(IConfig).IsAssignableFrom(m)).ToList();
                     if (configTypes.Any())
                     {
@@ -109,6 +116,8 @@ namespace NetModular.Lib.Config.Core
                                     }
 
                                     await _storage.Add(configDescriptor);
+
+                                    _logger.LogInformation("新增配置：{@key}", key);
                                 }
                             }
                         }
