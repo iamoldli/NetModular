a1b250d23b0fc0813a2ee9987a7cd7190f444e9d
OLDLI <wenju1991@163.com>
2020-03-17 10:37:37 +0800

    添加AutoMapper扩展

---
+++ src/Framework/Mapper/Mapper.AutoMapper/MapperConfigurationExpression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMapper
+{
+    public static class MapperConfigurationExpression
+    {
+        /// <summary>
+        /// 添加双向映射
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="cfg"></param>
+        public static void AddMap<TSource, TDestination>(this IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<TSource, TDestination>();
+            cfg.CreateMap<TDestination, TSource>();
+        }
+    }
+}
