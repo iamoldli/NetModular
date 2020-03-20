bf0681fa3f027fa6da25cb0571757d94c7aa7ee9
OLDLI <wenju1991@163.com>
2020-03-20 18:38:42 +0800

    修复审计日志无法解析未指定路由的bug

--- src/Admin/Web/Filters/AuditingHandler.cs
+++ src/Admin/Web/Filters/AuditingHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using NetModular.Lib.Auth.Abstractions;
 using NetModular.Lib.Auth.Web;
 using NetModular.Lib.Module.Abstractions;
@@ -23,13 +24,15 @@ namespace NetModular.Module.Admin.Web.Filters
         private readonly ILoginInfo _loginInfo;
         private readonly IAuditInfoService _auditInfoService;
         private readonly IModuleCollection _moduleCollection;
+        private readonly ILogger _logger;
 
-        public AuditingHandler(MvcHelper mvcHelper, ILoginInfo loginInfo, IAuditInfoService auditInfoService, IModuleCollection moduleCollection)
+        public AuditingHandler(MvcHelper mvcHelper, ILoginInfo loginInfo, IAuditInfoService auditInfoService, IModuleCollection moduleCollection, ILogger<AuditingHandler> logger)
         {
             _mvcHelper = mvcHelper;
             _loginInfo = loginInfo;
             _auditInfoService = auditInfoService;
             _moduleCollection = moduleCollection;
+            _logger = logger;
         }
 
         public async Task Hand(ActionExecutingContext context, ActionExecutionDelegate next)
@@ -43,58 +46,74 @@ namespace NetModular.Module.Admin.Web.Filters
 
             sw.Stop();
 
-            //执行结果
-            auditInfo.Result = JsonSerializer.Serialize(resultContext.Result);
-            //用时
-            auditInfo.ExecutionDuration = sw.ElapsedMilliseconds;
+            if (auditInfo != null)
+            {
+                try
+                {
+                    //执行结果
+                    auditInfo.Result = JsonSerializer.Serialize(resultContext.Result);
+                    //用时
+                    auditInfo.ExecutionDuration = sw.ElapsedMilliseconds;
 
-            await _auditInfoService.Add(auditInfo);
+                    await _auditInfoService.Add(auditInfo);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError("审计日志插入异常：{@ex}", ex);
+                }
+            }
         }
 
         private AuditInfoEntity CreateAuditInfo(ActionExecutingContext context)
         {
-            var routeValues = context.ActionDescriptor.RouteValues;
-            var auditInfo = new AuditInfoEntity
+            try
             {
-                AccountId = _loginInfo.AccountId,
-                AccountName = _loginInfo.AccountName,
-                Area = routeValues["area"] ?? "",
-                Controller = routeValues["controller"],
-                Action = routeValues["action"],
-                Parameters = JsonSerializer.Serialize(context.ActionArguments),
-                Platform = _loginInfo.Platform,
-                IP = _loginInfo.IP,
-                ExecutionTime = DateTime.Now
-            };
-
-            //获取模块的名称
-            if (auditInfo.Area.NotNull())
-            {
-                auditInfo.Module = _moduleCollection.FirstOrDefault(m => m.Id.EqualsIgnoreCase(auditInfo.Area))?.Name;
-            }
-
-            var controllerDescriptor = _mvcHelper.GetAllController().FirstOrDefault(m =>
-                m.Area.EqualsIgnoreCase(auditInfo.Area) && m.Name.EqualsIgnoreCase(auditInfo.Controller));
-            if (controllerDescriptor != null)
-            {
-                auditInfo.ControllerDesc = controllerDescriptor.Description;
-
-                var actionDescription = _mvcHelper.GetAllAction().FirstOrDefault(m =>
-                    m.Controller == controllerDescriptor && m.Name.EqualsIgnoreCase(auditInfo.Action));
-                if (actionDescription != null)
+                var routeValues = context.ActionDescriptor.RouteValues;
+                var auditInfo = new AuditInfoEntity
                 {
-                    auditInfo.ActionDesc = actionDescription.Description;
+                    AccountId = _loginInfo.AccountId,
+                    AccountName = _loginInfo.AccountName,
+                    Area = routeValues["area"] ?? "",
+                    Controller = routeValues["controller"],
+                    Action = routeValues["action"],
+                    Parameters = JsonSerializer.Serialize(context.ActionArguments),
+                    Platform = _loginInfo.Platform,
+                    IP = _loginInfo.IP,
+                    ExecutionTime = DateTime.Now
+                };
+
+                //获取模块的名称
+                if (auditInfo.Area.NotNull())
+                {
+                    auditInfo.Module = _moduleCollection.FirstOrDefault(m => m.Id.EqualsIgnoreCase(auditInfo.Area))?.Name;
                 }
-            }
 
-            //记录浏览器UA
-            if (_loginInfo.Platform == Platform.Web)
+                var controllerDescriptor = _mvcHelper.GetAllController().FirstOrDefault(m => m.Area.NotNull() && m.Area.EqualsIgnoreCase(auditInfo.Area) && m.Name.EqualsIgnoreCase(auditInfo.Controller));
+                if (controllerDescriptor != null)
+                {
+                    auditInfo.ControllerDesc = controllerDescriptor.Description;
+
+                    var actionDescription = _mvcHelper.GetAllAction().FirstOrDefault(m => m.Controller == controllerDescriptor && m.Name.EqualsIgnoreCase(auditInfo.Action));
+                    if (actionDescription != null)
+                    {
+                        auditInfo.ActionDesc = actionDescription.Description;
+                    }
+                }
+
+                //记录浏览器UA
+                if (_loginInfo.Platform == Platform.Web)
+                {
+                    auditInfo.BrowserInfo = context.HttpContext.Request.Headers["User-Agent"];
+                }
+
+                return auditInfo;
+            }
+            catch (Exception ex)
             {
-                auditInfo.BrowserInfo = context.HttpContext.Request.Headers["User-Agent"];
+                _logger.LogError("审计日志创建异常：{@ex}", ex);
             }
 
-            return auditInfo;
+            return null;
         }
-
     }
 }
