b89885f45c01338802cfa4411961dfdb1634f99e
OLDLI <wenju1991@163.com>
2020-04-01 09:57:48 +0800

    RabbitMQ添加Prefix属性

---
+++ test/Host/Host.Generic.Tests/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Host.Generic.Tests
+{
+    public class Startup : IHostedService
+    {
+        private readonly ILogger _logger;
+
+        public Startup(ILogger<Startup> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("服务开始：{@date}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("服务结束：{@date}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+    }
+}
