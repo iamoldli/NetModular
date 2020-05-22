4a97ea15ef6687db337ef7fee75169fd71c1cf1d
Oldli <wenju1991@163.com>
2020-05-22 11:29:29 +0800

    新增枚举选择radio和checkbox组件

---
+++ src/Admin/UI/module-admin/src/components/enum-checkbox/index.vue
@@ -0,0 +1,31 @@
+<script>
+import { mixins } from 'netmodular-ui'
+export default {
+  mixins: [mixins.checkbox],
+  data() {
+    return {
+      action: this.query
+    }
+  },
+  props: {
+    /**模块编码 */
+    moduleCode: {
+      type: String,
+      required: true
+    },
+    /**枚举名称 */
+    enumName: {
+      type: String,
+      required: true
+    },
+    /**所在库名称，如：Domain、Application等，默认Domain */
+    libName: String
+  },
+  methods: {
+    query() {
+      const { moduleCode, enumName, libName } = this
+      return $api.admin.tool.enumSelect({ moduleCode, enumName, libName })
+    }
+  }
+}
+</script>
